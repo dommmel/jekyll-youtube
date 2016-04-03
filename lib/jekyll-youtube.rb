f0ee8df804f5b05dd62a9afc25575ca3215170ea
Sheng-Loong Su <sushengloong@gmail.com>
2016-04-03 08:03:06 +0800

    Remove http protocol from embed src to fix https

--- lib/jekyll-youtube.rb
+++ lib/jekyll-youtube.rb
@@ -23,7 +23,7 @@ class YouTubeEmbed < Liquid::Tag
       site = context.registers[:site]
       tmpl = (Liquid::Template.parse tmpl).render site.site_payload.merge!({"youtube_id" => @youtube_id})
     else
-      %Q{<style>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style><div class='embed-container'>    <iframe title="YouTube video player" width="640" height="390" src="http://www.youtube.com/embed/#{ @youtube_id }" frameborder="0" allowfullscreen></iframe></div>}
+      %Q{<style>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style><div class='embed-container'>    <iframe title="YouTube video player" width="640" height="390" src="//www.youtube.com/embed/#{ @youtube_id }" frameborder="0" allowfullscreen></iframe></div>}
     end
   end
 
